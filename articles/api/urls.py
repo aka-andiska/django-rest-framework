7e19e695d45660ab06b2e56cbc27ceb3847c4965
aka <aka@junjies-MacBook-Pro.local>
2017-01-05 18:36:39 +0800

    more detail to output data(Article_List, Article_Delete, Article_Detail, Article_Update)

--- articles/api/urls.py
+++ articles/api/urls.py
@@ -2,14 +2,16 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from .views import (
+    ArticleDeleteAPIView,
 	ArticleDetailAPIView,
-    ArticleListAPIView
+    ArticleListAPIView,
+    ArticleUpdateAPIView,
 	)
 
 urlpatterns = [
 	url(r'^$', ArticleListAPIView.as_view(), name='list'),
     #url(r'^create/$', article_create),
     url(r'^(?P<slug>[\w-]+)/$', ArticleDetailAPIView.as_view(), name='detail'),
-    #url(r'^(?P<slug>[\w-]+)/edit/$', article_update, name='update'),
-    #url(r'^(?P<slug>[\w-]+)/delete/$', article_delete),
+    url(r'^(?P<slug>[\w-]+)/edit/$', ArticleUpdateAPIView.as_view(), name='update'),
+    url(r'^(?P<slug>[\w-]+)/delete/$', ArticleDeleteAPIView.as_view(), name='delete'),
 ]
--- articles/api/views.py
+++ articles/api/views.py
@@ -1,9 +1,25 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
-
+from rest_framework.generics import (
+    DestroyAPIView,
+    ListAPIView,
+    UpdateAPIView,
+    RetrieveAPIView,
+    )
 from articles.models import Article
 from .serializers import ArticleDetailSerializer, ArticListleSerializer
 
 class ArticleDetailAPIView(RetrieveAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleDetailSerializer
+    lookup_field = 'slug'
+    #lookup_url_kwarg = "abc"
+
+class ArticleUpdateAPIView(UpdateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleDetailSerializer
+    lookup_field = 'slug'
+    #lookup_url_kwarg = "abc"
+
+class ArticleDeleteAPIView(DestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleDetailSerializer
     lookup_field = 'slug'
