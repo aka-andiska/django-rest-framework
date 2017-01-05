e4a1b7f8f402cef2dc27e019c0d1512981eff8b9
aka <aka@junjies-MacBook-Pro.local>
2017-01-05 15:47:03 +0800

    Articles project: build CRUD blog with django REST Framework

---
+++ articles/api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework.serializers import ModelSerializer
+
+from articles.models import Article
+
+
+class ArticleSerializer(ModelSerializer):
+    class Meta:
+        model = Article
+        fields = [
+            'id',
+            'title',
+            'slug',
+            'content',
+            'publish',
+        ]
