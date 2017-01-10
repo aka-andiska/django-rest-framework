76cf05d51ed282d799ff2749e0334544bd29ac4c
aka-andiska <aka@junjies-MacBook-Pro.local>
2017-01-10 14:38:48 +0700

    Comment create Serializer function part 2

--- comments/api/serializers.py
+++ comments/api/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth import get_user_model
 
 from rest_framework.serializers import (
     ModelSerializer,
@@ -9,6 +10,7 @@ from rest_framework.serializers import (
 
 from comments.models import Comment
 
+User = get_user_model()
 def create_comment_serializer(model_type='post', slug=None, parent_id=None):
     class CommentCreateSerializer(ModelSerializer):
         class Meta:
@@ -39,6 +41,21 @@ def create_comment_serializer(model_type='post', slug=None, parent_id=None):
             if not obj_qs.exists() or obj_qs.count() !=1:
                 raise ValidationError("This is not a slug for this content type")
             return data
+
+        def create(self, validated_data):
+            content = validated_data.get("content")
+            user = User.objects.all().first()
+            model_type = self.model_type
+            slug = self.slug
+            parent_obj = self.parent_obj
+            comment = Comment.object.create_by_model_type(
+                    model_type=model_type,
+                    slug=slug,
+                    user=user,
+                    content=content,
+                    parent_obj=parent_obj,
+                    )
+            return comment
 
 
     return CommentCreateSerializer
